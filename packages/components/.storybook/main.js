a706bd5baa62b5cb1ecf0fa5cbf4e6bcd4e16d78
Axel Eriksson <axel.eriksson@symphony.com>
2023-05-05 17:28:52 +0200

    Storybook command works but crashes

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,36 +1,26 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.tsx'],
-  addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-a11y',
-    'storybook-addon-mdx-embed',
-    'storybook-dark-mode/register',
-    '@storybook/addon-controls',
-    '@storybook/preset-scss',
-    'storybook-addon-themes',
-  ],
-  webpackFinal: async (config) => {
-    config.module.rules.push(
-      {
-        test: /\.(ts|tsx)$/,
-        use: [
-          {
-            loader: require.resolve('ts-loader'),
-          },
-        ],
-      },
-      {
-        test: /\.md$/,
-        use: [
-          {
-            loader: 'markdown-loader',
-          },
-        ],
-      }
-    );
+  addons: ['@storybook/addon-docs', '@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-a11y', 'storybook-addon-mdx-embed', 'storybook-dark-mode/register', '@storybook/addon-controls', '@storybook/preset-scss', 'storybook-addon-themes', '@storybook/addon-mdx-gfm'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [{
+        loader: require.resolve('ts-loader')
+      }]
+    }, {
+      test: /\.md$/,
+      use: [{
+        loader: 'markdown-loader'
+      }]
+    });
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
   },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
